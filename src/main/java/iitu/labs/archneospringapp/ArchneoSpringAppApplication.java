e99f258a3b77b94b007a33b9db06007048b73cd8
Akzhol Shakterbekuly <Ashakterbekuly@1cb.kz>
2022-10-16 21:43:12 +0600

    initial commit

---
+++ src/main/java/iitu/labs/archneospringapp/ArchneoSpringAppApplication.java
@@ -0,0 +1,13 @@
+package iitu.labs.archneospringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchneoSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchneoSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iitu/labs/archneospringapp/ArchneoSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package iitu.labs.archneospringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchneoSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
