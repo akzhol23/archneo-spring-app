5b6017bff33d89c6695bc0cbecef58ff4d7f6102
Akzhol Shakterbekuly <Ashakterbekuly@1cb.kz>
2022-10-16 23:20:24 +0600

    change func name

--- src/main/java/iitu/labs/archneospringapp/controllers/MainController.java
+++ src/main/java/iitu/labs/archneospringapp/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String indexTitle(Model model) {
+    public String getHomePage(Model model) {
         model.addAttribute("title", "Main page");
         return "index";
     }
